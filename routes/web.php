61c82a4e3c314bde579a5e1af8426e4c483d83b1
Alexis1212asaxx <sanchezlopezfelixalexis1@.com>
2024-03-19 18:18:37 -0600

    subido

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DocenteController;
 use App\Http\Controllers\MateriaController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
